f3c9a03970e21a1922b631aaa6c777b8825faab7
Obi Knows <sam.nnodim@gmail.com>
2018-05-15 17:49:49 -0700

    added some vertical swipe code

--- App.js
+++ App.js
@@ -4,33 +4,80 @@ import { StyleSheet, Text, View } from "react-native";
 import { Container, Content } from "native-base";
 import  Swiper  from "react-native-swiper";
 
+const styles = StyleSheet.create({
+  slideDefault: {
+    flex:1,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:'#9DD6EB'
+  },
+  text:{
+    color:'white',
+    fontSize:30,
+    fontWeight:'bold',
+  },
+  subtext:{
+    color:'green',
+    fontSize:22,
+    fontWeight:'bold',
+  }
+})
+
 export default class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      outerScrollEnabled: true
+    }
+  }
+
+  verticalScroll = (index) => {
+    if(index !== 1){
+      this.setState({outerScrollEnabled:false})
+    } else {
+      this.setState({outerScrollEnabled:true})
+    }
+  }
+
   render() {
-    return (
-      <Container>
+    return <Container>
         <Content>
-          <Swiper>
-            <View>
-              <Text>Chat</Text>
+          <Swiper 
+            loop={false}
+            // showsPagination={false}
+            index={1}
+          >
+            
+            <View style={styles.slideDefault}>
+              <Text style={styles.text}>History & Chat</Text>
             </View>
-            <View>
-              <Text>Camera</Text>
-            </View>
-            <View>
-              <Text>Stories</Text>
+            <Swiper
+              loop={false}
+              horizontal={false}
+              showsPagination={false}
+              index={1}
+              onIndexChanged={(index) => this.verticalScroll(index)}
+            >
+              <View style={styles.slideDefault}>
+                <Text style={styles.text}>Profile & Settings</Text>
+              </View>
+
+              <View style={styles.slideDefault}>
+                <Text style={styles.text}>Camera</Text>
+              </View>
+
+              <View style={styles.slideDefault}>
+                <Text style={styles.text}>Retrieved Result</Text>
+              </View>
+            </Swiper>
+
+            <View style={styles.slideDefault}>
+              <Text style={styles.text}>Stories & Trends</Text>
+              <Text style={styles.subtext}>Kobo Earned</Text>
             </View>
           </Swiper>
         </Content>
-      </Container>
-    );
+      </Container>;
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
